c607f787e522e5fa94f2bf658f62922386eeee24
tryuichi <ryuichi.takahashi@gmail.com>
2018-03-20 08:24:37 +0900

    まずはDBに格納するところまでを

--- tips/save_info.py
+++ tips/save_info.py
@@ -1,2 +1,6 @@
 # TODO: 取得したxmlから必要な情報だけをDBに格納する。
+import xml.etree.ElementTree as ET
+tree = ET.parse('../references/api_result.xml')
+root = tree.getroot()
+print (root('version').text)
 # TODO: DBの選択 MongoDBにしよう。
